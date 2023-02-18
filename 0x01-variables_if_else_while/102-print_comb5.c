c3efbfd7e03e6cbc64a76a0f4292cca2c28a5eec
Kemo <sebatakgomokemo@gmail.com>
2023-02-18 03:43:37 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x < 100)
+	{
+		y = x;
+		while (y < 100)
+		{
+			putchar(x / 10 + 48);
+			putchar(x % 10 + 48);
+			putchar(' ');
+			putchar(y / 10 + 48);
+			putchar(y % 10 + 48);
+
+			if (x != 99 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		y++;
+		}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
