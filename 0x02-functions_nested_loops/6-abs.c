0555ea5fedce60eee512616465205031dd193ed1
Kemo <sebatakgomokemo@gmail.com>
2023-02-21 17:01:20 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Prototype function
+ *
+ * @num: is a value
+ *
+ * Return: num
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
