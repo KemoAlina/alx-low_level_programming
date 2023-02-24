6a925cc3ad5937437508b94e7d7fa084ddc86b85
Kemo <sebatakgomokemo@gmail.com>
2023-02-24 14:19:48 -0800

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,20 @@
 
 /**
  * _isupper - Entry point
+ *
+ * @c: value
+ *
+ * Return: 1 and 2
 */
 
-int _isupper(int c) {
-	if (c >= 'A' && c <= 'Z') {
-		return 1;
-	} else {
-		return 0;
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
