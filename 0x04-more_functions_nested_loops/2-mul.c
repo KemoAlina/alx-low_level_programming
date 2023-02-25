9c8d537a4fc0e1389020a291d2361374adda7fe9
Kemo <sebatakgomokemo@gmail.com>
2023-02-25 14:43:15 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: The first integer.
+ * @b: The second integer.
+ * Return: The sum of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
