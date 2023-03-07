1124397be6a9e8bb596b83ebae1aad20689fbe98
Kemo <sebatakgomokemo@gmail.com>
2023-03-06 16:33:32 -0800

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,20 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i;
+	int n = 0, m = 0, p = 1;
+	long int d1 = 0, d2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i * size + i);
-		sum2 += *(a + i * size + size - 1 - i);
+		d1 += a[(size * m) + m];
+		m++;
 	}
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		d2 += a[(size * n) + (size - p)];
+		n++;
+		p++;
+	}
+	printf("%ld, %ld\n", d1, d2);
 }
